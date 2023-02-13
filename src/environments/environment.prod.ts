9c50bc027daffa932cfd9e8e528444a656e0d80f
hiusanmaz <hiusanmaz34@gmail.com>
2023-02-13 14:09:15 +0300

    init ays-ui

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+const TIME_OUT = 3; // 3 days
+
+export const environment = {
+  production: true,
+  _baseUrl: 'Backend API URI',
+  envName: "Production",
+  apiUrl: "/api",
+  services: {},
+  session: TIME_OUT,
+};
